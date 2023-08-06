07faff61ff975954fd6a6044be3a21a42dd11576
양효령 <js980303@naver.com>
2023-08-07 08:51:56 +0900

    feat: 자동차는 랜덤 숫자가 4 이상이면 앞으로 전진하고 4 미만이면 정지한다.

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,6 +1,6 @@
 // - [x] 자동차는 이름을 가질 수 있다.
-// - [ ] 자동차는 랜덤 숫자가 4 이상이면 앞으로 전진한다.
-// - [ ] 자동차는 랜덤 숫자가 4 미만이면 정지한다.
+// - [x] 자동차는 랜덤 숫자가 4 이상이면 앞으로 전진한다.
+// - [x] 자동차는 랜덤 숫자가 4 미만이면 정지한다.
 
 import Car from "../src/Car";
 
@@ -10,5 +10,17 @@ describe("Car", () => {
   test("자동차는 이름을 가질 수 있다.", () => {
     const car = new Car(DEFAULT_CAR_NAME);
     expect(car.getName()).toBe(DEFAULT_CAR_NAME);
+  });
+
+  test("자동차는 랜덤 숫자가 4 이상이면 앞으로 전진한다.", () => {
+    const car = new Car(DEFAULT_CAR_NAME);
+    car.run(4);
+    expect(car.getPosition()).toBe(1);
+  });
+
+  test("자동차는 랜덤 숫자가 4 미만이면 정지한다.", () => {
+    const car = new Car(DEFAULT_CAR_NAME);
+    car.run(3);
+    expect(car.getPosition()).toBe(0);
   });
 });
--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,26 @@
+export const CAR_RUN_UNIT = 1;
+export const DEFAULT_POSITION = 0;
+export const RUN_THRESHOLD = 4;
+
 export default class Car {
   #name;
+  #position = DEFAULT_POSITION;
 
   constructor(name) {
     this.#name = name;
+  }
+
+  getName() {
+    return this.#name;
+  }
+
+  getPosition() {
+    return this.#position;
+  }
+
+  run(number) {
+    if (number >= RUN_THRESHOLD) {
+      this.#position += CAR_RUN_UNIT;
+    }
   }
 }
